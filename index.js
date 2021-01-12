fdadc07dc7942bda28acfd7c18696fdb3508f035
Sudipta Das <sudiptodas033@gmail.com>
2021-01-12 22:35:30 +0530

    infos

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ const io = require('socket.io')(server);
 const users = {};
 
 io.on('connection', socket =>{
+    //if any new user joins, broadcast to other users connected to this server.
     socket.on('new-user-joined', name =>{
         // console.log("New user", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
+    //if someone sends a msg..broadcast to all other users
     socket.on('send', message =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
 
+    //if someone leaves the chat..broadcast to all other users.
     socket.on('disconnect', message =>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
--- public/js/client.js
+++ public/js/client.js
@@ -7,6 +7,7 @@ const messageContainer = document.querySelector(".container");
 
 var audio = new Audio('receive.mp3');
 
+//function which will append event info to the container.
 const append = (message, position)=>{
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
@@ -18,6 +19,7 @@ const append = (message, position)=>{
     }
 }
 
+//after submit the form, send the info to server.
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
@@ -30,20 +32,24 @@ form.addEventListener('submit', (e)=>{
     messageInput.value = ''
 })
 
+//ask new user for his/her name and let the server know.
 const name = prompt("enter your name to join");
 socket.emit('new-user-joined', name);
 
+//if a new user joins, receive the event from the server.
 socket.on('user-joined', name =>{
     append(`${name} joined the chat`, 'left');
     messageContainer.scrollTop = messageContainer.scrollHeight;
 })
 
+//if server sends a message, receive it.
 socket.on('receive', data =>{
     
     append(`${data.name}: ${data.message}`, 'left');
     messageContainer.scrollTop = messageContainer.scrollHeight;
 })
 
+//if a user left the chat, append the info to the container.
 socket.on('left', name =>{
     append(`${name} left the chat`, 'left');
     messageContainer.scrollTop = messageContainer.scrollHeight;
